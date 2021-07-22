e558f1341a14fe681f51869c860811038a91f61e
Student209 <85950195+Student209@users.noreply.github.com>
2021-07-21 20:43:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import logging
 
 import coloredlogs
 
+
 from Coach import Coach
-from othello.OthelloGame import OthelloGame as Game
-from othello.pytorch.NNet import NNetWrapper as nn
-from utils import *
+from connect4.Connect4Game import Connect4Game
+from connect4.tensorflow.NNet import NNetWrapper as nn
+from utils import dotdict
+
 
 log = logging.getLogger(__name__)
 
